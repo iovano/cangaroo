529d44c4925c351b63784dfe038d2bea0d53a368
Timor Kodal <timor@kodal.de>
2023-04-03 01:05:25 +0200

    introduce and implement "static" and "blendMode"

--- public/js/classes/Sprite.js
+++ public/js/classes/Sprite.js
@@ -1,6 +1,9 @@
 class Sprite {
     style = ''
     frame = 0;
+    static = false;
+    blendMode = 'source-over';
+    clip = false; // clip === true : alias for "blendMode='source-in"
     constructor(shape = "rect", x = null, y = null, width = null, height = null, speedX = null, speedY = null, red = null, green = null, blue = null, alpha = null) {
         this.shape = shape;
         this.x = x;
@@ -14,11 +17,18 @@ class Sprite {
         this.sx = speedX;
         this.sy = speedY;
     }
+    setStatic(value) {
+        this.static = value;
+    }
+    isStatic() {
+        return this.static === true;
+    }
     setStyle(value) {
         this.style = value;
     }
     draw(context) {
         this.frame ++;
+        context.globalCompositeOperation = this.clip === true ? 'source-in' : this.blendMode;
         context.fillStyle = "rgba("+this.r+", "+this.g+", "+this.b+", "+this.a+")";
         if (this.shape === "ball") {
             context.beginPath();
@@ -26,12 +36,18 @@ class Sprite {
                 context.arc(this.x, this.y, this.w / 2, 0, 2 * Math.PI);
             }
             context.fill();
+            context.closePath();
         } else if (Array.isArray(this.shape)) {
             context.font = this.style;
             context.fillText(this.shape[Math.floor(Math.random()*this.shape.length)], this.x, this.y);
-        } else {
+        } else if (this.shape == "rect") {
             context.fillRect(this.x + this.w / 2, this.y + this.w / 2, this.w, this.h);
+        } else if (this.shape instanceof Image) {
+            if (this.shape.complete && this.shape.naturalHeight !==0) {
+                context.drawImage(this.shape, this.x ?? 0, this.y ?? 0);
+            }
         }
+        context.globalCompositeOperation = 'source-over';
     }
 
 }
